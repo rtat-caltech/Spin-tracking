93ed9111aa3248bd1338caaec2a85372970ef86e
Raymond Tat <rtat@caltech.edu>
2023-05-31 09:17:48 -0700

    Changed indentation in integrator.cpp

--- src/integrator.cpp
+++ src/integrator.cpp
@@ -19,17 +19,17 @@ using namespace std;
 
 __PREPROC__ double sign(double a, double b)
 {
-  return (b < 0.0)? -abs(a) : abs(a);
+	return (b < 0.0)? -abs(a) : abs(a);
 }
 
 __PREPROC__ double min_d(double a, double b)
 {
-  return (a < b)?a:b;
+	return (a < b)?a:b;
 }
 
 __PREPROC__ double max_d(double a, double b)
 {
-  return (a > b)?a:b;
+	return (a > b)?a:b;
 }
 
 __PREPROC__ double3 pulse(const double t){
@@ -45,7 +45,7 @@ __PREPROC__ double3 grad(double3& pos){
 
 __PREPROC__ void obs_dense(long nr, double xold, double x, double3 y, double3 pos_old, double3 v_old, int* irtrn, options opts, 
 		double* lastOutput, unsigned int *lastIndex, outputDtype* outputArray, double hout, double3& rcont1, double3& rcont2, 
-        double3& rcont3, double3& rcont4, double3& rcont5, double3& rcont6, double3& rcont7, double3& rcont8){
+		double3& rcont3, double3& rcont4, double3& rcont5, double3& rcont6, double3& rcont7, double3& rcont8){
 	////("%ld %lf %lf %lf %lf\n", nr, x, y.x, y.y, y.z);
 	////("%lf %lf %lf %lf\n", *lastOutput, xold, x, opts.ioutInt);
 	double s;
@@ -105,8 +105,8 @@ __PREPROC__ void interpolate(const double t, const double t0, const double tf,
 }
 
 __PREPROC__ double3 findCrossTerm(const double t, const double3 y, const double3 B0, const double3 E, 
-					 const double gamma, const double t0, const double tf, const double3 p_old,
-					 const double3 p_new, const double3 v_old, const double3 v_new){
+		const double gamma, const double t0, const double tf, const double3 p_old,
+		const double3 p_new, const double3 v_old, const double3 v_new){
 	double3 p, v, G;
 	double3 B = pulse(t) + B0;
 	interpolate(t,t0,tf,p_old,p_new,v_old,v_new,p,v);
@@ -116,8 +116,8 @@ __PREPROC__ double3 findCrossTerm(const double t, const double3 y, const double3
 }
 
 __PREPROC__ void Bloch(const double t, const double3& y, double3& f, const double3 B0, const double3 E,
-			const double gamma, const double t0, const double tf , const double3& p_old,
-			const double3& p_new, const double3& v_old, const double3& v_new){
+		const double gamma, const double t0, const double tf , const double3& p_old,
+		const double3& p_new, const double3& v_old, const double3& v_new){
 	double3 temp;
 	temp = findCrossTerm(t, y, B0, E, gamma, t0, tf, p_old, p_new, v_old, v_new);
 	f = cross(y, temp);
@@ -180,144 +180,144 @@ __PREPROC__ int integrateDOP(double t0, double tf, double3& y, const double3& p_
 		const double3& p_new, const double3& v_old, const double3& v_new, 
 		options OPT){
 
-    double3 yy1, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10;
-    //double3 rcont1, rcont2, rcont3, rcont4, rcont5, rcont6, rcont7, rcont8;
-    //int arret, idid;
-    //int iasti, iord, irtrn, reject, last, nonsti;
-    int reject, last;
+	double3 yy1, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10;
+	//double3 rcont1, rcont2, rcont3, rcont4, rcont5, rcont6, rcont7, rcont8;
+	//int arret, idid;
+	//int iasti, iord, irtrn, reject, last, nonsti;
+	int reject, last;
 	double facold, expo1, fac, facc1, facc2, fac11, posneg, xph;
-    double err2, deno;
+	double err2, deno;
 	double3 erri, sqr, sk;
-    double atoli, rtoli, hlamb, err, hnew;
-    unsigned int nfcn = 0, nstep = 0, naccpt = 0, nrejct = 0;
-    double x = t0;
-    double xf = tf;
-    double h = OPT.h;
-    int n = 3;
+	double atoli, rtoli, hlamb, err, hnew;
+	unsigned int nfcn = 0, nstep = 0, naccpt = 0, nrejct = 0;
+	double x = t0;
+	double xf = tf;
+	double h = OPT.h;
+	int n = 3;
 
-    facold = 1.0E-4;
-    expo1 = 1.0/8.0 - OPT.beta * 0.2;
-    facc1 = 1.0 / OPT.fac1;
-    facc2 = 1.0 / OPT.fac2;
-    posneg = sign(1.0, tf-t0);
+	facold = 1.0E-4;
+	expo1 = 1.0/8.0 - OPT.beta * 0.2;
+	facc1 = 1.0 / OPT.fac1;
+	facc2 = 1.0 / OPT.fac2;
+	posneg = sign(1.0, tf-t0);
 
-    /* initial preparations */
-    atoli = OPT.atol;
-    rtoli = OPT.rtol;
-    last  = 0;
-    hlamb = 0.0;
+	/* initial preparations */
+	atoli = OPT.atol;
+	rtoli = OPT.rtol;
+	last  = 0;
+	hlamb = 0.0;
 	////("k1 prior = %lf %lf %lf\n", k1.x, k1.y, k1.z);
-    Bloch(x, y, k1, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+	Bloch(x, y, k1, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 	////("k1 post = %lf %lf %lf\n", k1.x, k1.y, k1.z);
 
-    double hmax = std::abs(OPT.hmax);
-    // if (OPT.h == 0.0)
-    //     h = hinit(fcn, x0, y, posneg, k1, k2, k3, iord, hmax, OPT.atol, OPT.rtol);
-    nfcn += 2;
-    reject = 0;
+	double hmax = std::abs(OPT.hmax);
+	// if (OPT.h == 0.0)
+	//     h = hinit(fcn, x0, y, posneg, k1, k2, k3, iord, hmax, OPT.atol, OPT.rtol);
+	nfcn += 2;
+	reject = 0;
 	/*
-    if (OPT.iout){
-        irtrn = 1;
-        hout = 1.0;
-        xout = t0;
-        obs(naccpt+1, xold, x, y, p_old, &irtrn, OPT);
-    }*/
+	  if (OPT.iout){
+	  irtrn = 1;
+	  hout = 1.0;
+	  xout = t0;
+	  obs(naccpt+1, xold, x, y, p_old, &irtrn, OPT);
+	  }*/
 
-    while (1){
+	while (1){
 
-        if (nstep > OPT.nmax){
-            return -2;
-        }
+		if (nstep > OPT.nmax){
+			return -2;
+		}
 
-        if (0.1 * std::abs(h) <= std::abs(x) * OPT.uround){
-            return -3;
-        }
+		if (0.1 * std::abs(h) <= std::abs(x) * OPT.uround){
+			return -3;
+		}
 
-        if ((x + 1.01*h - xf) * posneg > 0.0){
-            h = xf - x;
-            last = 1;
-        }
+		if ((x + 1.01*h - xf) * posneg > 0.0){
+			h = xf - x;
+			last = 1;
+		}
 
-        nstep++;
+		nstep++;
 
-        /* the twelve stages */
+		/* the twelve stages */
 		////("yy1 = %lf %lf %lf\n", yy1.x, yy1.y, yy1.z);
-        yy1 = y + h * COEF::a21 * k1;
-		////("yy12 = %lf %lf %lf\n", yy1.x, yy1.y, yy1.z);
+		yy1 = y + h * COEF::a21 * k1;
+		////("Yy12 = %lf %lf %lf\n", yy1.x, yy1.y, yy1.z);
 
-        Bloch(x+COEF::c2*h, yy1, k2, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
-		
-        yy1 = y + h * (COEF::a31*k1 + COEF::a32*k2);
-        Bloch(x+COEF::c3*h, yy1, k3, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		Bloch(x+COEF::c2*h, yy1, k2, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        yy1 = y + h * (COEF::a41*k1 + COEF::a43*k3);
-        Bloch(x+COEF::c4*h, yy1, k4, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		yy1 = y + h * (COEF::a31*k1 + COEF::a32*k2);
+		Bloch(x+COEF::c3*h, yy1, k3, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        yy1 = y + h * (COEF::a51*k1 + COEF::a53*k3 + COEF::a54*k4);
-        Bloch(x+COEF::c5*h, yy1, k5, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		yy1 = y + h * (COEF::a41*k1 + COEF::a43*k3);
+		Bloch(x+COEF::c4*h, yy1, k4, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        yy1 = y + h * (COEF::a61*k1 + COEF::a64*k4 + COEF::a65*k5);
-        Bloch(x+COEF::c6*h, yy1, k6, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		yy1 = y + h * (COEF::a51*k1 + COEF::a53*k3 + COEF::a54*k4);
+		Bloch(x+COEF::c5*h, yy1, k5, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        yy1 = y + h * (COEF::a71*k1 + COEF::a74*k4 + COEF::a75*k5 + COEF::a76*k6);
-        Bloch(x+COEF::c7*h, yy1, k7, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		yy1 = y + h * (COEF::a61*k1 + COEF::a64*k4 + COEF::a65*k5);
+		Bloch(x+COEF::c6*h, yy1, k6, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        yy1 = y + h * (COEF::a81*k1 + COEF::a84*k4 + COEF::a85*k5 + COEF::a86*k6 + COEF::a87*k7);
-        Bloch(x+COEF::c8*h, yy1, k8, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		yy1 = y + h * (COEF::a71*k1 + COEF::a74*k4 + COEF::a75*k5 + COEF::a76*k6);
+		Bloch(x+COEF::c7*h, yy1, k7, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        yy1 = y + h * (COEF::a91*k1 + COEF::a94*k4 + COEF::a95*k5 + COEF::a96*k6 + COEF::a97*k7 + COEF::a98*k8);
-        Bloch(x+COEF::c9*h, yy1, k9, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		yy1 = y + h * (COEF::a81*k1 + COEF::a84*k4 + COEF::a85*k5 + COEF::a86*k6 + COEF::a87*k7);
+		Bloch(x+COEF::c8*h, yy1, k8, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        yy1 = y + h * (COEF::a101*k1 + COEF::a104*k4 + COEF::a105*k5 + COEF::a106*k6 + COEF::a107*k7 + COEF::a108*k8 + COEF::a109*k9);
-        Bloch(x+COEF::c10*h, yy1, k10, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		yy1 = y + h * (COEF::a91*k1 + COEF::a94*k4 + COEF::a95*k5 + COEF::a96*k6 + COEF::a97*k7 + COEF::a98*k8);
+		Bloch(x+COEF::c9*h, yy1, k9, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        yy1 = y + h * (COEF::a111*k1 + COEF::a114*k4 + COEF::a115*k5 + COEF::a116*k6 + COEF::a117*k7 + COEF::a118*k8 + COEF::a119*k9 + COEF::a1110*k10);
+		yy1 = y + h * (COEF::a101*k1 + COEF::a104*k4 + COEF::a105*k5 + COEF::a106*k6 + COEF::a107*k7 + COEF::a108*k8 + COEF::a109*k9);
+		Bloch(x+COEF::c10*h, yy1, k10, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
 
-        Bloch(x+COEF::c11*h, yy1, k2, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
-        xph = x + h;
+		yy1 = y + h * (COEF::a111*k1 + COEF::a114*k4 + COEF::a115*k5 + COEF::a116*k6 + COEF::a117*k7 + COEF::a118*k8 + COEF::a119*k9 + COEF::a1110*k10);
 
-        yy1 = y + h * (COEF::a121*k1 + COEF::a124*k4 + COEF::a125*k5 + COEF::a126*k6 + COEF::a127*k7 + COEF::a128*k8 + COEF::a129*k9 + COEF::a1210*k10 + COEF::a1211*k2);
+		Bloch(x+COEF::c11*h, yy1, k2, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		xph = x + h;
 
-        Bloch(xph, yy1, k3, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
-        nfcn += 11;
+		yy1 = y + h * (COEF::a121*k1 + COEF::a124*k4 + COEF::a125*k5 + COEF::a126*k6 + COEF::a127*k7 + COEF::a128*k8 + COEF::a129*k9 + COEF::a1210*k10 + COEF::a1211*k2);
 
-        k4 = COEF::b1*k1 + COEF::b6*k6 + COEF::b7*k7 + COEF::b8*k8 + COEF::b9*k9 + COEF::b10*k10 + COEF::b11*k2 + COEF::b12*k3;
-		
-        k5 = y + h * k4;
-        
-        /* error estimation */
-        err = 0.0;
-        err2 = 0.0;
+		Bloch(xph, yy1, k3, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+		nfcn += 11;
+
+		k4 = COEF::b1*k1 + COEF::b6*k6 + COEF::b7*k7 + COEF::b8*k8 + COEF::b9*k9 + COEF::b10*k10 + COEF::b11*k2 + COEF::b12*k3;
+
+		k5 = y + h * k4;
+
+		/* error estimation */
+		err = 0.0;
+		err2 = 0.0;
 		sk = atoli + rtoli * max_d3(fabs3(y), fabs3(k5));
 		erri = k4 - COEF::bhh1*k1 - COEF::bhh2*k9 - COEF::bhh3*k3;
-        sqr = erri / sk;
-        err2 += sum(sqr*sqr);
-        erri = COEF::er1*k1 + COEF::er6*k6 + COEF::er7*k7 + COEF::er8*k8 + COEF::er9*k9 +
-            COEF::er10 * k10 + COEF::er11*k2 + COEF::er12*k3;
-        sqr = erri / sk;
-        err += sum(sqr*sqr);
+		sqr = erri / sk;
+		err2 += sum(sqr*sqr);
+		erri = COEF::er1*k1 + COEF::er6*k6 + COEF::er7*k7 + COEF::er8*k8 + COEF::er9*k9 +
+			COEF::er10 * k10 + COEF::er11*k2 + COEF::er12*k3;
+		sqr = erri / sk;
+		err += sum(sqr*sqr);
 
-        deno = err + 0.01 * err2;
-        if (deno <= 0.0)
+		deno = err + 0.01 * err2;
+		if (deno <= 0.0)
 			deno = 1.0;
-        err = std::abs(h) * err * sqrt (1.0 / (deno*(double)n));
+		err = std::abs(h) * err * sqrt (1.0 / (deno*(double)n));
 
-        /* computation of hnew */
-        fac11 = pow (err, expo1);
-        /* Lund-stabilization */
-        fac = fac11 / pow(facold,OPT.beta);
-        /* we require fac1 <= hnew/h <= fac2 */
-        fac = max_d (facc2, min_d (facc1, fac/OPT.safe));
-        hnew = h / fac;
+		/* computation of hnew */
+		fac11 = pow (err, expo1);
+		/* Lund-stabilization */
+		fac = fac11 / pow(facold,OPT.beta);
+		/* we require fac1 <= hnew/h <= fac2 */
+		fac = max_d (facc2, min_d (facc1, fac/OPT.safe));
+		hnew = h / fac;
 
-        if (err <= 1.0)
-        {
-            /* step accepted */
+		if (err <= 1.0)
+		{
+			/* step accepted */
 
-            facold = max_d (err, 1.0E-4);
-            naccpt++;
-            Bloch(xph, k5, k4, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
-            nfcn++;
+			facold = max_d (err, 1.0E-4);
+			naccpt++;
+			Bloch(xph, k5, k4, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
+			nfcn++;
 			/*
 			// final preparation for dense output
 			if (OPT.iout == 2)
@@ -339,7 +339,7 @@ __PREPROC__ int integrateDOP(double t0, double tf, double3& y, const double3& p_
 					COEF::d79*k9 + COEF::d710*k10 + COEF::d711*k2 + COEF::d712*k3;
 
 				//the next three function evaluations 
-			   yy1 = y + h * (COEF::a141*k1 + COEF::a147*k7 + COEF::a148*k8 +
+				yy1 = y + h * (COEF::a141*k1 + COEF::a147*k7 + COEF::a148*k8 +
 					COEF::a149*k9 + COEF::a1410*k10 + COEF::a1411*k2 +
 					COEF::a1412*k3 + COEF::a1413*k4);
 				Bloch(x+COEF::c14*h, yy1, k10, OPT.B0, OPT.E, OPT.gamma, t0, tf, p_old, p_new, v_old, v_new);
@@ -383,30 +383,30 @@ __PREPROC__ int integrateDOP(double t0, double tf, double3& y, const double3& p_
 					return 2;
 			}*/
 
-            // normal exit
-            if (last)
-            {
-                return 1;
-            }
+			// normal exit
+			if (last)
+			{
+				return 1;
+			}
 
-            if (std::abs(hnew) > hmax)
-                hnew = posneg * hmax;
-            if (reject)
-                hnew = posneg * min_d (std::abs(hnew), std::abs(h));
+			if (std::abs(hnew) > hmax)
+				hnew = posneg * hmax;
+			if (reject)
+				hnew = posneg * min_d (std::abs(hnew), std::abs(h));
 
-            reject = 0;
-        }
-        else{
-            /* step rejected */
-            hnew = h / min_d (facc1, fac11/OPT.safe);
-            reject = 1;
-            if (naccpt >= 1)
+			reject = 0;
+		}
+		else{
+			/* step rejected */
+			hnew = h / min_d (facc1, fac11/OPT.safe);
+			reject = 1;
+			if (naccpt >= 1)
 				nrejct=nrejct + 1;
-            last = 0;
-        }
+			last = 0;
+		}
 
-        h = hnew;
-    }
+		h = hnew;
+	}
 
 }
 
@@ -419,7 +419,7 @@ __PREPROC__ int integrateRK45Hybrid(double t0, double tf, double3& y, const doub
 	double hmin = 1.0E-9;
 	int nstep = 0;
 	double endOfSimulDt;
-	
+
 	double3 k1;
 	double3 k2;
 	double3 k3;
@@ -525,3 +525,4 @@ __PREPROC__ int integrateRK45Hybrid(double t0, double tf, double3& y, const doub
 	}
 	return 0;
 }
+
