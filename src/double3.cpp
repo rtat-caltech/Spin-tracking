1022b828f859d8c0b36dbca4d04fbef5e0d5d993
Mathews, David <mathewsdg@ornl.gov>
2023-06-19 21:04:57 +0000

    updating to add the new rodriguez

--- src/double3.cpp
+++ src/double3.cpp
@@ -171,3 +171,11 @@ __PREPROC__ quaternion rodriguezQuat(const double3 k, const double dt){
 	norm = norm * -sin(h/2.0);
 	return {cos(h/2.0), norm.x, norm.y, norm.z};
 }
+
+__PREPROC__ double3 rodriguez(const double3 k, const double3 v1){
+	double angle = len(k);
+	double s =  sin(angle);
+	double c = cos(angle);
+	return v1 * c + (cross(v1, k) * (s/angle)) + k * (dot(k, v1) * (1.0 - c)/(angle * angle));
+}
+
